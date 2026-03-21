f07586dee247893382e4511920754478b8a92e9f
MohammadAshour <36931318+MohammadAshour@users.noreply.github.com>
2026-03-22 00:10:09 +0200

    Update route.js

--- app/api/ingredients/route.js
+++ app/api/ingredients/route.js
@@ -1,4 +1,3 @@
-
 import dbConnect from '../../../lib/db';
 import Ingredient from '../../../models/Ingredient';
 import { NextResponse } from 'next/server';
@@ -8,6 +7,17 @@ export async function GET() {
     await dbConnect();
     const ingredients = await Ingredient.find({});
     return NextResponse.json({ success: true, data: ingredients });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message }, { status: 400 });
+  }
+}
+
+export async function POST(request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const ingredient = await Ingredient.create(body);
+    return NextResponse.json({ success: true, data: ingredient }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ success: false, error: error.message }, { status: 400 });
   }
