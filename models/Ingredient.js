333b1dff829cbb9eb56a829a768c549ebc7626bf
MohammadAshour <36931318+MohammadAshour@users.noreply.github.com>
2026-03-22 01:14:10 +0200

    Update Ingredient.js

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const IngredientSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: String,
-  unit: String,
-});
+  quantity: { type: Number, default: 0 }, // Ensure it's Number
+  unit: { type: String, required: true },
+}, { timestamps: true });
 
 export default mongoose.models.Ingredient || mongoose.model('Ingredient', IngredientSchema);
