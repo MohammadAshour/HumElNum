3c5c2f730f09ab3a5a132063921a5ff084b9da5c
mahmoud-ashour624 <mahmmoudashour5@gmail.com>
2026-03-21 23:39:20 +0200

    x2

--- x/Ingredient.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const IngredientSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  category: String,
-  unit: String,
-});
-
-export default mongoose.models.Ingredient || mongoose.model('Ingredient', IngredientSchema);
